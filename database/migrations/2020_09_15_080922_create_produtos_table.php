5f51d30fcf38d965429de7e8049ecf25f64c5a28
Heitor Goncalves <heitor-goncalves@hotmail.com>
2020-09-15 08:40:40 -0300

    Criando tabela produto

---
+++ database/migrations/2020_09_15_080922_create_produtos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nomeProduto', 100);
+            $table->integer('sku');
+            $table->float('peso');
+            $table->float('altura');
+            $table->float('largura');
+            $table->float('profundidade');
+            $table->decimal('valor', 4, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
